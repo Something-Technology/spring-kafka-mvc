f1431d8421532a4a483fae1115953e9742560c2c
Aurelia Hengst <ahengst.ext@thatsit.services>
2021-05-18 09:00:56 +0200

    added evaluation for topic provider

--- src/main/java/technology/something/spring/kafka/MessageController.java
+++ src/main/java/technology/something/spring/kafka/MessageController.java
@@ -1,11 +1,15 @@
 package technology.something.spring.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import technology.something.spring.kafka.services.impl.DefaultKafkaMessageHandlerService;
 import technology.something.spring.kafka.services.impl.DefaultKafkaMethodReturnValueHandlerService;
 import technology.something.spring.kafka.services.impl.DefaultKafkaTopicProvider;
 
 public abstract class MessageController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MessageController.class);
 
     @Autowired
     protected DefaultKafkaTopicProvider topicProvider;
@@ -15,4 +19,12 @@ public abstract class MessageController {
 
     @Autowired
     protected DefaultKafkaMethodReturnValueHandlerService methodReturnValueHandlerService;
+
+    protected boolean isTopicProviderAvailable() {
+        if (topicProvider == null) {
+            LOG.error("Topic provider not available.");
+            return false;
+        }
+        return true;
+    }
 }
